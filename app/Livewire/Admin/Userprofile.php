95ac83b5d006715b5ce5fa276231c99eca80fb64
Moscow12 <meshackstev@gmail.com>
2025-04-15 09:20:01 +0300

    feat:adding user profile

--- app/Livewire/Admin/Userprofile.php
+++ app/Livewire/Admin/Userprofile.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire\Admin;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Userprofile extends Component
 {
+    public $user;
     public function render()
     {
         return view('livewire.admin.userprofile');
+    }
+
+    public function mount()
+    {
+        $this->user = Auth::user();
     }
 }
